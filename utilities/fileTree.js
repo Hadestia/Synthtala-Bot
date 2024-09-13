ed31a24fedb84b4fdd893db6afe1a14c21d798a9
Hadestia <rjchristianroldan@gmail.com>
2024-09-13 12:47:02 +0800

    updated file(s)

--- utilities/fileTree.js
+++ utilities/fileTree.js
@@ -1,11 +1,11 @@
 // FileTree : Will automatically make default folders and subfolders if not existed.
 const { existsSync } = require('fs-extra');
 const { exec } = require('child_process');
-const path = require('path');
+const Path = require('path');
 
 async function makeFileTree (obj, basePath, CLIENT, Logger) {
 	Object.keys(obj).forEach(function(key) {
-		let path = path.normalize(basePath + '/' + key);
+		let path = Path.normalize(basePath + '/' + key);
 		if (!existsSync(path)) {
 			exec(`mkdir ${path}`, (error, stdout, stderr) => {
 				if (error) {
